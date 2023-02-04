3ecdb49c88400dd1961b5ff3f055509113cc61ce
Pratik Jain <pratikjain227@gmail.com>
2023-02-04 12:38:54 +0530

    Added @Tableau and #DataFam to config query

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # This is hastag which Twitter bot will search and retweet. You can edit this with any hastag .For example : '#javascript'
 
-QUERY = '#Tableau'
+QUERY = '#Tableau OR #DataFam OR @Tableau'
 
 # Twitter bot setting for liking Tweets
 LIKE = True 
